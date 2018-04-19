e1b769d8d6ce43fb8cd392e58d3fd2307bb94019
Bogdan <bci@1stweb.dk>
2018-04-19 15:19:08 +0200

    add index file and php logic

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+// Load the XML source
+$xml = new DOMDocument;
+// Create a stream
+function download_page($path){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,$path);
+    curl_setopt($ch, CURLOPT_USERAGENT, "PHP/".PHP_VERSION );
+    curl_setopt($ch, CURLOPT_FAILONERROR,1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION,1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 15);
+    $retValue = curl_exec($ch);          
+    curl_close($ch);
+    return $retValue;
+}
+// Open the file using the HTTP headers set above
+$domain = $_GET["source"];
+$guid = $_GET["guid"];
+$stack = [];
+
+if($domain && $guid) {
+  foreach ($guid as &$value) {
+    $file = download_page($domain . $value);
+    $xml=simplexml_load_string($file);
+    array_push($stack, array(
+       "name" => (string)$xml->SagsNr, 
+       "guid" => $value,
+       "image" => (string)$xml->Media->Fotos->Foto[0]->ProtectedFilename,
+       "title" => (string)$xml->Marketing->AnnonceOverskrift,
+       "labelText" => (string)$xml->Marketing->LabelTekst->Tekst,
+       "finance" => $xml->Finansiering,
+       "property" => $xml->Ejendom
+    ));
+  }
+
+  echo json_encode($stack);
+}
+else {
+  echo "missing domain and guid params";
+}
